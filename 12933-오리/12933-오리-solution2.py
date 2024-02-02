bc6c8593e45ee6dec28f1a95bef2ba50540aa808
Soojin-Lee-01 <sojinlee1004@naver.com>
2024-02-02 15:18:31 +0900

    12933-오리 update

---
+++ 12933-오리/12933-오리-solution2.py
@@ -0,0 +1,35 @@
+import sys
+
+duck = sys.stdin.readline().rstrip()
+visited = [False] * len(duck)
+count = 0
+
+if len(duck) % 5 != 0 or duck[0] != 'q':
+    print(-1)
+    exit()
+
+def solve(start):
+    global count
+    quack = 'quack'
+    j = 0
+    first = True
+    for i in range(start, len(duck)):
+        if duck[i] == quack[j] and not visited[i]:
+            visited[i] = True
+            if duck[i] == 'k':
+                if first:
+                    count += 1
+                    first = False
+                j = 0
+                continue
+            j += 1
+
+
+for i in range(len(duck)):
+    if duck[i] == 'q' and not visited[i]:
+        solve(i)
+
+if not all(visited) or count == 0:
+    print(-1)
+else:
+    print(count)
