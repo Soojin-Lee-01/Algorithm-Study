6ab7adffb97a55758a1a4264b821488dece4a84b
soojinlee <sojinlee1004@naver.com>
2024-02-16 11:32:12 +0900

    1912-연속합 update

---
+++ 1912-연속합/1912-연속합.py
@@ -0,0 +1,10 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+data = list(map(int, sys.stdin.readline().rstrip().split(' ')))
+
+for i in range(1, n):
+    data[i] = max(data[i], data[i] + data[i-1])
+
+print(max(data))
