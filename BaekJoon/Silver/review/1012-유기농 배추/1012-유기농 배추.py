d79f670ee1833f607d47fbc582692c5905dc849b
soojinlee <sojinlee1004@naver.com>
2024-03-13 14:57:49 +0900

    1012-유기농 배추 review

--- BaekJoon/Silver/review/1012-유기농 배추/1012-유기농 배추.py
+++ BaekJoon/Silver/review/1012-유기농 배추/1012-유기농 배추.py
@@ -4,9 +4,10 @@ from collections import deque
 test = int(sys.stdin.readline())
 
 def bfs(r, c, grap, visi):
-    visi[r][c] = True
     queue = deque()
     queue.append((r, c))
+    visi[r][c] = True
+
     directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
 
     while queue:
@@ -15,31 +16,30 @@ def bfs(r, c, grap, visi):
             next_r = cur_r + dr
             next_c = cur_c + dc
             if next_r >= 0 and next_r < len(graph)and next_c >= 0 and next_c < len(graph[0]):
-                if grap[next_r][next_c] == 1 and visi[next_r][next_c] is not True:
-                    queue.append((next_r, next_c))
-                    visi[next_r][next_c] = True
-
-
+                if grap[next_r][next_c] == 1:
+                    if visi[next_r][next_c] is False:
+                        queue.append((next_r, next_c))
+                        visi[next_r][next_c] = True
 result = []
 for i in range(test):
     count = 0
-    m, n, k = map(int, sys.stdin.readline().split(' '))
+    m, n, number = map(int, sys.stdin.readline().rstrip().split(' '))
+
     graph = [[0 for _ in range(m)] for _ in range(n)]
+
     visited = [[False for _ in range(m)] for _ in range(n)]
-    for j in range(k):
-        a, b = map(int, sys.stdin.readline().split(' '))
+
+    for j in range(number):
+        a, b = map(int, sys.stdin.readline().rstrip().split(' '))
         graph[b][a] = 1
-    for a in range(len(graph)):
-        for b in range(len(graph[0])):
-            if graph[a][b] == 1 and not visited[a][b]:
-                bfs(a, b, graph, visited)
+
+    for k in range(n):
+        for l in range(m):
+            if graph[k][l] == 1 and not visited[k][l]:
                 count += 1
+                bfs(k, l, graph, visited)
     result.append(count)
+
 
 for i in result:
     print(i)
-
-
-
-
-
