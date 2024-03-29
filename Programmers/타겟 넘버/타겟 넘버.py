2789ac2a3e0bfc15be5f79dc15855fa328b73535
soojinlee <sojinlee1004@naver.com>
2024-03-30 01:08:53 +0900

    타겟 넘버 update

---
+++ Programmers/타겟 넘버/타겟 넘버.py
@@ -0,0 +1,8 @@
+from itertools import product
+
+def solution(numbers, target):
+    l = [(x, -x) for x in numbers]
+    s = list(map(sum, product(*l)))
+    return s.count(target)
+
+solution([1, 1, 1, 1, 1], 3)
