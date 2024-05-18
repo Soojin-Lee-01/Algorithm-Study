a7222e795e89f5bbb66c6dc5d48c743b9dd0af48
soojinlee <sojinlee1004@naver.com>
2024-05-18 22:00:47 +0900

    11729 - 하노이 탑 이동 순서 update

---
+++ BaekJoon/Python/Gold/update/11729-하노이 탑 이동 순서/11729-하노이 탑 이동 순서.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+def hanoi(n, start, end):
+    if n == 1:
+        print(start, end)
+        return
+    hanoi(n-1, start, 6-end-start)
+    print(start, end)
+    hanoi(n-1, 6-end-start, end)
+
+
+print(2**n-1)
+hanoi(n, 1, 3)
+
