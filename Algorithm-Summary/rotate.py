4506d12db9fe6d3d936f23cabde9d4c6ab009082
Lee-SooJin <87466284+Soojin-Lee-01@users.noreply.github.com>
2024-06-13 09:44:35 +0900

    Update rotate.py

--- Algorithm-Summary/rotate.py
+++ Algorithm-Summary/rotate.py
@@ -32,6 +32,16 @@ def rotate_270(list_2d):
             new[m-j-1][i] = list_2d[i][j]
     
     return new
+# 90도 반시계 회전
+def rotate_90_counter(list_2d):
+    n = len(list_2d)
+    m = len(list_2d[0])
+    new = [[0] * n for _ in range(m)]
+    for i in range(n):
+        for j in range(m):
+            new[m-j-1][i] = list_2d[i][j]
+    
+    return new
 
 # Solution 2)
 
@@ -39,3 +49,7 @@ def rotate_270(list_2d):
 def rotated_90(array_2d):
     list_of_tuples = zip(*array_2d[::-1])
     return [list(e) for e in list_of_tuples]
+
+# 90도 반시계 회전
+def rotate_90_counter(list_2d):
+    return [list(row)[::-1] for row in zip(*list_2d)]
