e2e684678f8638007a0b28cfa771bfa910a0a9c7
Soojin-Lee-01 <sojinlee1004@naver.com>
2024-07-09 13:03:56 +0900

    15686 - 치킨 배달 review2

--- BaekJoon/Python/Gold/review/15686-치킨 배달/15686-치킨 배달.py
+++ BaekJoon/Python/Gold/review/15686-치킨 배달/15686-치킨 배달.py
@@ -1,65 +1,49 @@
-# 도시의 크기와 정할 치킨집 갯수
 n, m = map(int, input().split())
 
-# 도시 그래프
 graph = []
 
-# 치킨 집 위치, 집의 위치
+for i in range(n):
+    data = list(map(int, input().split()))
+    graph.append(data)
+
 chicken = []
 house = []
 
-for i in range(n):
-    data = list(map(int, input().split(' ')))
-    graph.append(data)
-
+# 치킨의 좌표와 집의 좌표를 모두 담는다.
 for i in range(n):
     for j in range(n):
-
-        # 치킨 집 위치 넣기
         if graph[i][j] == 2:
             chicken.append((i, j))
-
-        # 집 위치 넣기
-        elif graph[i][j] == 1:
+        if graph[i][j] == 1:
             house.append((i, j))
 
-# 조합 모두 담기
+# 치킨집의 조합
 result = []
-
-
-# 치킨집의 경우의 수 담기
-def combi(n, start=0, combo=[]):
-    if len(combo) == n:
+def combination(num, start = 0, combo =[]):
+    if len(combo) == num:
         result.append(combo[:])
         return
     for i in range(start, len(chicken)):
         combo.append(chicken[i])
-        combi(n, i + 1, combo)
+        combination(num, i+1, combo)
         combo.pop()
 
-    return result
+combination(m)
 
+min_route = 1e9
 
-min_value = int(1e9)
+# 치킨 집의 조합을 모두 비교해서 최소값을 찾는다.
+for i in range(len(result)):
+    temp = result[i]
+    m = 0
+    for j in range(len(house)):
+        h_x, h_y = house[j]
+        m_h = 1e9
+        for k in temp:
+            c_x, c_y = k
+            route = abs(h_x-c_x) + abs(h_y-c_y)
+            m_h = min(route, m_h)
+        m += m_h
+    min_route = min(m, min_route)
 
-
-# 최소 거리 구하기 2
-def find_dis(c):
-    global min_value
-    all_c = 0
-    for h_x, h_y in house:
-        min_c = 2 * n + 1
-        for dr, dc in c:
-            dist = (abs(h_x - dr) + abs(h_y - dc))
-            min_c = min(min_c, dist)
-
-        all_c += min_c
-
-    min_value = min(min_value, all_c)
-
-
-# 최소 거리 구하기 1
-for c in combi(m):
-    find_dis(c)
-
-print(min_value)
+print(min_route)
