33c9862bf8ece0e708693adcf7465d5e6736a6c3
Soojin-Lee-01 <sojinlee1004@naver.com>
2024-07-09 17:08:23 +0900

    21610 - 마법사 상어와 비바라기 review

--- BaekJoon/Python/Gold/review/21610-마법사 상어와 비바라기/21610-마법사 상어와 비바라기.py
+++ BaekJoon/Python/Gold/review/21610-마법사 상어와 비바라기/21610-마법사 상어와 비바라기.py
@@ -1,65 +1,48 @@
-N, M = map(int, input().split())
+n, m = map(int, input().split())
+
 graph = []
-for _ in range(N):
+for i in range(n):
     data = list(map(int, input().split()))
     graph.append(data)
-cloud = {(N-1, 0), (N-1, 1), (N-2, 0), (N-2, 1)}
-dx = [0, -1, -1, -1, 0, 1, 1, 1]
-dy = [-1, -1, 0, 1, 1, 1, 0, -1]
 
-def move(d, s): # 비구름이 있는 곳
-    global cloud
-    new_cloud = set()
-    for dr, dc in cloud:
-        for _ in range(s):
-            dr = dr + dx[d-1]
-            dc = dc + dy[d-1]
-            if dr == -1:
-                dr = N -1
-            if dr == N:
-                dr = 0
-            if dc == -1:
-                dc = N-1
-            if dc == N:
-                dc = 0
-        new_cloud.add((dr, dc))
-        cloud = new_cloud
+cloud = [(n-1, 0), (n-1, 1), (n-2, 0), (n-2, 1)]
 
-def upgrade():
-    global cloud
-    for dr, dc in cloud:
-        graph[dr][dc] += 1
-    for dr, dc in cloud:
-        temp = 0
-        for i in range(1, 8, 2):
-            next_r = dr + dx[i]
-            next_c = dc + dy[i]
-            if 0 <= next_r < N and 0 <= next_c < N:
-                if graph[next_r][next_c] > 0:
-                    temp += 1
-        graph[dr][dc] += temp
+directions = [(0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1)]
+di = [(-1, 1), (1, -1), (1, 1), (-1, -1)]
 
-def find():
-    global cloud
-    temp = set()
-    for i in range(N):
-        for j in range(N):
-            if (i, j) not in cloud:
-                if graph[i][j] >= 2:
-                    temp.add((i, j))
-    cloud = temp
-    for dr, dc in cloud:
-        graph[dr][dc] -= 2
-
-for i in range(M):
+for i in range(m):
     d, s = map(int, input().split())
-    move(d, s)
+    for j in range(len(cloud)):
+        x, y = cloud[j]
+        for _ in range(s):
+            x = (x + directions[d - 1][0]) % n
+            y = (y + directions[d - 1][1]) % n
+        cloud[j] = (x, y)
 
-    upgrade()
-    find()
+    for x, y in cloud:
+        graph[x][y] += 1
+    temp_cloud = cloud.copy()
+    cloud.clear()
+    for cur_r, cur_c in temp_cloud:
+        water = 0
+        for dr, dc in di:
+            next_r = cur_r + dr
+            next_c = cur_c + dc
+            if 0 <= next_r < n and 0 <= next_c < n:
+                if graph[next_r][next_c] > 0:
+                    water += 1
+        graph[cur_r][cur_c] += water
 
-result = 0
-for i in range(N):
-    for j in range(N):
-        result += graph[i][j]
-print(result)
+    for a in range(len(graph)):
+        for b in range(len(graph[0])):
+            if (a, b) not in temp_cloud:
+                if graph[a][b] >= 2:
+                    cloud.append((a, b))
+                    graph[a][b] -= 2
+    temp_cloud.clear()
+final = 0
+for i in range(len(graph)):
+    for j in range(len(graph[0])):
+        final += graph[i][j]
+
+print(final)
