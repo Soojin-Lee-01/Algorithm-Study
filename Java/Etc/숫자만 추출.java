078334c8442c17df06e46452155e19ea5b01370f
Lee-SooJin <87466284+Soojin-Lee-01@users.noreply.github.com>
2024-09-26 16:55:24 +0900

    숫자만 추출 - 2 update
    문자열 문제

--- Java/Etc/숫자만 추출.java
+++ Java/Etc/숫자만 추출.java
@@ -16,6 +16,26 @@ public class Main {
         return answer;
     }
 
+    public int solution2 (String str) {
+        String answer = "";
+
+        for (char x : str.toCharArray()) {
+            if (Character.isDigit(x)) answer += x;
+        }
+
+        return Integer.parseInt(answer);
+    }
+
+    public int solution3 (String str) {
+        int answer = 0;
+
+        for (char x : str.toCharArray()) {
+            if (x >= 48 && x <= 57) answer = answer * 10 + (x-48);
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         Main T = new Main();
         Scanner scanner = new Scanner(System.in);
